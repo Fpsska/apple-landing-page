fa34983e2f26eb3efc684477ebeff80eb4a2b0ff
Makeilokoogehena <studworkse@gmail.com>
2024-05-21 22:06:34 +0300

    FEAT: create logic for mobile menu section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -35,10 +35,40 @@ const slider = new Swiper(".swiper", {
 
 // ./ SLIDER
 
-const menuButton = document.querySelector(".menu-button");
-const menuClose = document.querySelector(".menu-close");
-const menuList = document.querySelector(".menu__mobile-list");
+const menuButton = document.querySelector(".menu__button");
+const menuClose = document.querySelector(".menu-mobile__button-close");
+const menuMobile = document.querySelector(".menu-mobile");
 
-menuButton.addEventListener("click", () => {
-  menuList.classList.add("menu__mobile-list-open");
-});
+//
+
+function menuMobileButtonOpen() {
+  menuMobile.classList.add("menu-mobile__list-open");
+}
+
+function onDocumentClick(event) {
+  if (!menuMobile.classList.contains("menu-mobile__list-open")) return;
+  if (menuMobile.contains(event.target) || menuButton.contains(event.target)) {
+    return;
+  }
+  menuMobileButtonClose();
+}
+
+function menuMobileButtonClose() {
+  menuMobile.classList.remove("menu-mobile__list-open");
+}
+
+function onDocumentKeydown(e) {
+  if (!menuMobile.classList.contains("menu-mobile__list-open")) return;
+  if (e.code === "Escape") menuMobileButtonClose();
+}
+
+function onWindowResize() {
+  if (!menuMobile.classList.contains("menu-mobile__list-open")) return;
+  if (window.innerWidth >= 662) menuMobileButtonClose();
+}
+
+menuButton.addEventListener("click", menuMobileButtonOpen);
+menuClose.addEventListener("click", menuMobileButtonClose);
+document.addEventListener("keydown", (e) => onDocumentKeydown(e));
+document.addEventListener("click", (event) => onDocumentClick(event));
+window.addEventListener("resize", onWindowResize);
